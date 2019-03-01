12c8a024db5fc610b0adaa8d541cbd01b542d36d
aleksey-ilin <ilinalekseys@gmail.com>
2019-03-01 22:01:16 +0300

    add libs, configs

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+
+// eslint-disable-next-line react/prefer-stateless-function
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>ENEKcalc</h1>
+      </div>
+    );
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index-bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
