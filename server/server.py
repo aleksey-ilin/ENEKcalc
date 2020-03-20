5fbe256c19be95cfe5e3a230e2cbdcba2c0df504
aleksey-ilin <ilinalekseys@gmail.com>
2020-03-20 19:10:56 +0300

    add urllib.parse to server

--- server/server.py
+++ server/server.py
@@ -1,5 +1,11 @@
+from urllib.parse import urlparse, parse_qs
+
+
 def app(environ, start_response):
     """Simplest possible application object"""
+    parsed_url = urlparse(environ['RAW_URI'])
+    query = parse_qs(parsed_url.query)
+    print(query)
     data = b'Hello, World!\n'
     status = '200 OK'
     response_headers = [
