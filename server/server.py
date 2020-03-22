836d52f4f882c53e00919fb5bfbc96706cd57c0c
aleksey-ilin <ilinalekseys@gmail.com>
2020-03-22 17:36:29 +0300

    fis server

--- server/server.py
+++ server/server.py
@@ -1,12 +1,22 @@
 from urllib.parse import urlparse, parse_qs
+from pyXSteam.XSteam import XSteam
 
 
 def app(environ, start_response):
-    """Simplest possible application object"""
+    print(environ)
     parsed_url = urlparse(environ['RAW_URI'])
     query = parse_qs(parsed_url.query)
-    print(query)
-    data = b'Hello, World!\n'
+    func = list(query)[0]
+    args = list(query[func])
+    steamTable = XSteam(XSteam.UNIT_SYSTEM_MKS)
+    getResult = getattr(steamTable, func)
+
+    if len(args) == 1:
+        result = getResult(int(args[0]))
+    elif len(args) == 2:
+        result = getResult(int(args[0]), int(args[1]))
+
+    data = bytes("result = {}".format(result), "utf-8")
     status = '200 OK'
     response_headers = [
         ('Content-type', 'text/plain'),
