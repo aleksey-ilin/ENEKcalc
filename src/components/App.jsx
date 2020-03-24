66280669ba6f656c6f827ae66e0d6010f988521e
aleksey-ilin <ilinalekseys@gmail.com>
2020-03-24 12:19:07 +0300

    fix eslint error, eslint rules

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,18 +7,24 @@ const App = () => {
 
   const getPressure = () => {
   // fetch('https://api.enek.dotterian.ru/?h_pt=180&h_pt=18')
-  fetch(`https://api.enek.dotterian.ru/?psat_t=${temperature}`)
-    .then(response => response.json())
-    .then(pressure => setPressure(pressure))
-  }
+    fetch(`https://api.enek.dotterian.ru/?psat_t=${temperature}`)
+      .then(response => response.json())
+      .then(pressureSaturation => setPressure(pressureSaturation));
+  };
 
   return (
     <div className={styles.root}>
       <h1>Параметры на линии насыщения</h1>
-      <label htmlFor="temperature">Температура, &ordm;С</label>
-      <input id ="temperature" type="number" onChange={e => setTemperature(e.target.value)} />
+      <label htmlFor="temperature">
+        Температура, &ordm;С
+        <input type="number" id="temperature" onChange={e => setTemperature(e.target.value)} />
+      </label>
       <button type="button" onClick={getPressure}>рассчитать давление</button>
-      <p>Давление = {pressure} бар</p>
+      <p>
+        {'Давление = '}
+        {pressure}
+        {' бар'}
+      </p>
     </div>
   );
 };
