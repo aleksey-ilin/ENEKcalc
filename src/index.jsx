b11a979a1cb62e29d87ff121b094652f76dfa611
aleksey-ilin <ilinalekseys@gmail.com>
2020-03-25 19:25:59 +0300

    add ErrorBoundary

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import ErrorBoundary from './components/ErrorBoundary';
 import './index.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ErrorBoundary><App /></ErrorBoundary>, document.getElementById('root'));
