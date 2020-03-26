da7c722842e922131a736dad65bffa292cff2e9f
aleksey-ilin <ilinalekseys@gmail.com>
2020-03-26 12:19:21 +0300

    fix eslint rules

--- src/components/ErrorBoundary.jsx
+++ src/components/ErrorBoundary.jsx
@@ -8,14 +8,16 @@ export default class ErrorBoundary extends React.Component {
   }
 
   render() {
-    if (this.state.errorInfo) {
+    const { error, errorInfo } = this.state;
+
+    if (errorInfo) {
       return (
         <div>
           <h2>Something went wrong.</h2>
           <details style={{ whiteSpace: 'pre-wrap' }}>
-            {this.state.error && this.state.error.toString()}
+            {error && error.toString()}
             <br />
-            {this.state.errorInfo.componentStack}
+            {errorInfo.componentStack}
           </details>
         </div>
       );
