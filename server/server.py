5bb179a4067f8c2c5b6da82284935a81f9ab889f
aleksey-ilin <ilinalekseys@gmail.com>
2020-04-22 15:56:07 +0300

    edit server

--- server/server.py
+++ server/server.py
@@ -1,27 +1,18 @@
-# from urllib.parse import urlparse, parse_qs
+from urllib.parse import urlparse, parse_qs
 from iapws import IAPWS97
-# from pyXSteam.XSteam import XSteam
 import json
 
 
 def app(environ, start_response):
-    # parsed_url = urlparse(environ['RAW_URI'])
-    # query = parse_qs(parsed_url.query)
-    # func = list(query)[0]
-    # args = list(query[func])
+    parsed_url = urlparse(environ['RAW_URI'])
+    query = parse_qs(parsed_url.query)
+    func1 = list(query)[0]
+    args1 = list(query[func1])
+    func2 = list(query)[1]
+    args2 = list(query[func2])
     sat_steam = IAPWS97(P=1, x=1)
-    sat_liquid = IAPWS97(T=370, x=0)
-    steam = IAPWS97(P=2.5, T=500)
-    print(sat_steam.h, sat_liquid.h, steam.h)
-    # steamTable = XSteam(XSteam.UNIT_SYSTEM_MKS)
-    # getResult = getattr(steamTable, func)
 
-    # if len(args) == 1:
-    #     result = getResult(int(args[0]))
-    # elif len(args) == 2:
-    #     result = getResult(int(args[0]), int(args[1]))
-
-    result = 124
+    result = sat_steam.h
     status = '200 OK'
     response_headers = [
         ('Content-type', 'application/json;charset=utf-8'),
